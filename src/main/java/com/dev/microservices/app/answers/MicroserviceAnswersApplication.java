c7b9b5784bb52177f04388fb3b15bdcab3011ea6
Erick Estrada <erickestradapj@gmail.com>
2021-12-04 16:54:44 -0500

    starting project

---
+++ src/main/java/com/dev/microservices/app/answers/MicroserviceAnswersApplication.java
@@ -0,0 +1,13 @@
+package com.dev.microservices.app.answers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceAnswersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceAnswersApplication.class, args);
+	}
+
+}
